2963a1e7b4b71bf3c8d50287ed22959345dc12c2
Ram <Ram@gmail.ac.in>
2022-10-14 15:22:18 +0530

    -updated getqueryset

---
+++ dispatching/get.py
@@ -0,0 +1,10 @@
+def get_queryset(self):
+    if DispatcherRoles.BROKER in self.request.user.get_all_roles:
+        raise RestValidationError(
+                detail={"message":"You don't have access to this api."},
+                code=status.HTTP_403_FORBIDDEN
+            )
+    dcm = getattr(self.request.user, "dispatchcompanymembership", None)
+    # print("DCMMMMMMMMM", dcm)
+    if dcm:
+        user_role = dcm.roles.all()
